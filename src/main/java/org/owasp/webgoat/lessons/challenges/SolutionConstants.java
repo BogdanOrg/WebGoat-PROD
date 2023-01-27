20170edad5df24c2701884f15beca36372f4fbc5
bogdan <bogdan.ganzha@mend.io>
2023-01-27 16:27:52 +0100

    one more change

--- src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
@@ -30,7 +30,4 @@ package org.owasp.webgoat.lessons.challenges;
  */
 public interface SolutionConstants {
 
-
-    //TODO should be random generated when starting the server
-    String PASSWORD = "!!webgoat_admin_1234!!";
 }
