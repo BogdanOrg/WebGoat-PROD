cf8fa97384abad03b1b91495bda3b16f0838f19f
bogdan <bogdan.ganzha@mend.io>
2023-01-27 16:16:08 +0100

    add empty line

--- src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
@@ -30,6 +30,7 @@ package org.owasp.webgoat.lessons.challenges;
  */
 public interface SolutionConstants {
 
+
     //TODO should be random generated when starting the server
     String PASSWORD = "!!webgoat_admin_1234!!";
     String PASSWORD_TOM = "thisisasecretfortomonly";
