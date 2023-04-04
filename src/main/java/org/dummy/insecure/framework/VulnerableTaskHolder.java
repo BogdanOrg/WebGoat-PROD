527373ff7ec5a58937850a63c3288cdd72b65705
bogdanganzha <121808191+bogdanganzha@users.noreply.github.com>
2023-04-04 10:31:05 +0200

    Update VulnerableTaskHolder.java

--- src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
+++ src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 //TODO move back to lesson
 public class VulnerableTaskHolder implements Serializable {
     
+    
 	private static final long serialVersionUID = 2;
     
 	private String taskName;
